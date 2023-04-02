42ef1685d3994465f2830482dbda193d398bc16c
Nithish Raghava <127293733+beamJPEG@users.noreply.github.com>
2023-04-02 13:05:00 +0800

    Add files via upload

---
+++ n.py
@@ -0,0 +1,65 @@
+program = []
+
+while True:
+    # Get user input
+    user_input = input("> ")
+    
+    # Check for keywords and execute corresponding actions
+    if user_input.startswith("print "):
+        # Extract text from user input
+        text = user_input.split("print ", 1)[1]
+        # Print text to console
+        print(text)
+        
+    elif user_input == "end":
+        # End program
+        break
+    
+    elif user_input == "start":
+        # Start program
+        program = []
+        
+    elif user_input.startswith("var "):
+        # Extract variable name and value from user input
+        _, var_name, var_value = user_input.split()
+        # Add variable to program
+        program.append((var_name, var_value))
+        
+    elif user_input.endswith(" run"):
+        # Extract variable name from user input
+        var_name = user_input.split(" run")[0]
+        # Find variable in program
+        for var in program:
+            if var[0] == var_name:
+                # Set variable value to True
+                program.remove(var)
+                program.append((var_name, True))
+                break
+                
+    elif user_input.endswith(" unrun"):
+        # Extract variable name from user input
+        var_name = user_input.split(" unrun")[0]
+        # Find variable in program
+        for var in program:
+            if var[0] == var_name:
+                # Set variable value to False
+                program.remove(var)
+                program.append((var_name, False))
+                break
+                
+    elif user_input.startswith("if "):
+        # Extract variable name and value from user input
+        _, var_name, var_value = user_input.split()
+        # Find variable in program
+        for var in program:
+            if var[0] == var_name:
+                # Execute if statement if variable value is True
+                if var[1]:
+                    print(f"If statement is true: {var_value}")
+                # Otherwise, execute else statement
+                else:
+                    print("Else statement")
+                break
+                
+    else:
+        print("Invalid input")
